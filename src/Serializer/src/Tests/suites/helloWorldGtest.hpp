e7f6686c9fbed0a9b7ec0a6848025914900af686
DESKTOP-NJFUO3C\saul <saceves@urbanissa.mx>
2021-11-05 17:40:44 -0600

    google test inegration progress

---
+++ src/Serializer/src/Tests/suites/helloWorldGtest.hpp
@@ -0,0 +1,14 @@
+#ifndef HELLOWORLDGTEST
+#define HELLOWORLDGTEST
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(MAP_SERIALIZER, HelloWorld) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+#endif
